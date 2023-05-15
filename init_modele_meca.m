6c98c7e5914712484febd6e2e7987a38b0553bec
Eugene S <104144598+hlebovod@users.noreply.github.com>
2023-05-15 11:42:53 +0200

    Add files via upload

---
+++ init_modele_meca.m
@@ -0,0 +1,7 @@
+Cme	 =  100;%	N.m
+I_M  = 	0.2;%	kg.m²
+I_BV = 	0.1; % kg.m²
+b_M  = 	0.1; % N.m.s.rad-1
+b_BV = 	0.1; %	N.m.s.rad-1
+k    =  0.5; %  (rapport démul TF)	su (sans unité)
+k_E  =  0.5; % (rapport commande emb)	su
